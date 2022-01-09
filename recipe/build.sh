c6228815f3ceca35fe21c33660744de319140cf7
Uwe L. Korn <uwe.korn@quantco.com>
2022-01-09 21:10:55 +0100

    Set conda compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 yarn pack
 yarn licenses generate-disclaimer --production > ThirdPartyLicenses.txt
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
-npm install --build-from-source -g parcel-v${PKG_VERSION}.tgz
+npm install --os_linux_compiler=${CC} --build-from-source -g parcel-v${PKG_VERSION}.tgz
